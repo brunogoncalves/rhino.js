a80782f18f791d4dd35f5f0516ce9cac397253f8
bruno.goncalves <bugotech@gmail.com>
2021-09-22 12:02:31 -0300

    add ajuste

--- webcore/middlewares/totop.js
+++ webcore/middlewares/totop.js
@@ -6,7 +6,11 @@
 module.exports = (router) => {
 
     router.beforeEach((to, from, next) => {
-        window.scrollTo(0, 0);
+
+        // So fazer scroll se não tem hash (ancora)
+        if (to.hash == '') {
+            window.scrollTo(0, 0);
+        }
 
         return next();
     });

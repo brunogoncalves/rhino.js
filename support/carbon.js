52ba568dd4b3b4e4e07975068fe2aa1e789de855
bruno.goncalves <bugotech@gmail.com>
2022-03-30 15:29:43 -0300

    correçaõ do last day week e month

--- support/carbon.js
+++ support/carbon.js
@@ -4,37 +4,6 @@ if (!(typeof _dt.parse == 'function')) {
     _dt = _dt.default;
 }
 
-_dt.extend({
-    formatter: {
-
-        // Extend LD (Ultimo dia do mês)
-        LL: function (d) {
-
-            // O Dia ZERO do mê sé o ultimo dia
-            var ld = new Date(d.getFullYear(), d.getMonth() + 1, 0);
-
-            return _dt.format(ld, 'DD');
-        },
-
-        // Extend FW (Primeiro dia da semana)
-        W: function (d) {
-            var diff = d.getDate() - d.getDay();
-            var nd = new Date(d.setDate(diff));
-
-            return _dt.format(nd, 'DD');
-        },
-
-        // Extend FW (Ultimo dia da semana)
-        WW: function (d) {
-            var diff = d.getDate() - d.getDay() + 6;
-            var nd = new Date(d.setDate(diff));
-
-            return _dt.format(nd, 'DD');
-        }
-
-    }
-});
-
 class Carbon
 {
     /**
@@ -201,6 +170,56 @@ class Carbon
         dt2 = (typeof dt2 == 'number') ? new Date(dt2) : dt2;
 
         return _dt.subtract(dt1, dt2);
+    }
+
+    /**
+     * Retorna a data do primeiro dia da semana.
+     * 
+     * @param {Date} d Data base
+     * @param {Boolean} utc De deve tratar como UTC
+     * @returns {Date}
+     */
+    static getFirstDayWeek(d, utc = true) {
+        if (utc) {
+            var diff = d.getUTCDate() - d.getUTCDay();
+            return new Date(d.setUTCDate(diff));
+        } else {
+            var diff = d.getDate() - d.getDay();
+            return new Date(d.setDate(diff));
+        }
+    }
+
+    /**
+     * Retorna a data do último dia da semana.
+     * 
+     * @param {Date} d Data base
+     * @param {Boolean} utc De deve tratar como UTC
+     * @returns {Date}
+     */
+    static getLastDayWeek(d, utc = true) {
+        if (utc) {
+            var diff = d.getUTCDate() - d.getUTCDay() + 6;
+            return new Date(d.setUTCDate(diff));
+        } else {
+            var diff = d.getDate() - d.getDay() + 6;
+            return new Date(d.setDate(diff));
+        }
+    }
+
+    /**
+     * Retorna a data do último dia da mês.
+     * 
+     * @param {Date} d Data base
+     * @param {Boolean} utc De deve tratar como UTC
+     * @returns {Date}
+     */
+    static getLastDayMonth(d, utc = true) {
+        // O Dia ZERO do mê sé o ultimo dia
+        if (utc) {
+            return new Date(d.getUTCFullYear(), d.getUTCMonth() + 1, 0);
+        } else {
+            return new Date(d.getFullYear(), d.getMonth() + 1, 0);
+        }
     }
 
     /**
--- tests/support/carbon.test.js
+++ tests/support/carbon.test.js
@@ -4,14 +4,29 @@ test('Carbon - inicio e final do mes', () => {
 
     var dia = carbon.createFromFormat('2022-03-15', 'YYYY-MM-DD');
 
+    var du = carbon.getLastDayMonth(dia);
+
     expect(carbon.format(dia, 'YYYY-MM-[01]')).toStrictEqual('2022-03-01');
-    expect(carbon.format(dia, 'YYYY-MM-LL')).toStrictEqual('2022-03-31');
+    expect(carbon.format(du, 'YYYY-MM-DD')).toStrictEqual('2022-03-31');
 });
 
 test('Carbon - inicio e final da semana', () => {
 
     var dia = carbon.createFromFormat('2022-03-15', 'YYYY-MM-DD');
 
-    expect(carbon.format(dia, 'YYYY-MM-W')).toStrictEqual('2022-03-13');
-    expect(carbon.format(dia, 'YYYY-MM-WW')).toStrictEqual('2022-03-19');
+    var d1 = carbon.getFirstDayWeek(dia);
+    var d2 = carbon.getLastDayWeek(dia);
+
+    expect(carbon.format(d1, 'YYYY-MM-DD')).toStrictEqual('2022-03-13');
+    expect(carbon.format(d2, 'YYYY-MM-DD')).toStrictEqual('2022-03-19');
+
+    // Com virada de mês
+
+    var dia = carbon.createFromFormat('2022-03-30', 'YYYY-MM-DD');
+
+    var d1 = carbon.getFirstDayWeek(dia);
+    var d2 = carbon.getLastDayWeek(dia);
+
+    expect(carbon.format(d1, 'YYYY-MM-DD')).toStrictEqual('2022-03-27');
+    expect(carbon.format(d2, 'YYYY-MM-DD')).toStrictEqual('2022-04-02');
 });
